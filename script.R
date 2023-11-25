0f2da25fe060556c8c4d020f50fa5e07a10c28dd
Samuel Schmahl <105804551+LordSmaul@users.noreply.github.com>
2023-11-25 17:19:14 -0600

    Update script.R
    Includes code for primary decision tree model + performance metrics and plot visualization.

--- script.R
+++ script.R
@@ -1,5 +1,6 @@
 library(MASS)
 library(rpart)
+library(rpart.plot)
 library(tidymodels)
 library(tidyr)
 library(readr)
@@ -23,16 +24,31 @@ data_split <- initial_split(master_u, prop=0.8)
 train_data <- training(data_split)
 test_data <- testing(data_split)
 
-# Build model, fit it with predicted data, and build confusion matrix
+# Build baseline model, fit it with predicted data, and build confusion matrix
 model <- rpart(Target~., data=train_data, method='class')
 predict_test <- predict(model, test_data, type='class')
 conf_mat <- table(test_data$Target, predict_test)
 
-# Model and data performance measures
+# Model and data performance measures for baseline model
 precision <- conf_mat[1] / sum(conf_mat[1], conf_mat[3])
 recall <- conf_mat[1] / sum(conf_mat[1], conf_mat[2])
 f1 <- 2 * ((precision * recall) / (precision + recall))
 accuracy <- sum(diag(conf_mat)) / sum(conf_mat)
 
-# Plot of the decision tree model
+# Plot of the baseline decision tree model
 rpart.plot(model, tweak=1.3, cex=0.4)
+
+
+# Build primary model, fit it with predicted data, and build confusion matrix
+model_b <- rpart(Target~., data=train_data, method='class', maxdepth=6)
+predict_test_b <- predict(model_b, test_data, type='class')
+conf_mat_b <- table(test_data$Target, predict_test_b)
+
+# Model and data performance measures for primary model
+precision_b <- conf_mat_b[1] / sum(conf_mat_b[1], conf_mat_b[3])
+recall_b <- conf_mat_b[1] / sum(conf_mat_b[1], conf_mat_b[2])
+f1_b <- 2 * ((precision_b * recall_b) / (precision_b + recall_b))
+accuracy_b <- sum(diag(conf_mat_b)) / sum(conf_mat_b)
+
+# Plot of the primary decision tree model
+rpart.plot(model_b, tweak=1.3, cex=0.4)
